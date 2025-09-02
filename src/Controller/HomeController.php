dd7cf9ad2c01b4e07fe6556fccfddb08310f2cea
otoneumann <otarshenge@gmail.com>
2025-09-02 17:24:54 +0200

    Twig bundle added

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-class HomeController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
 {
+    #[Route('/')]
+    public function index(): Response
+    {
+     return $this->render('home/index.html.twig');
+    }
 
 }
